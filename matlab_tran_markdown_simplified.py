e7c5018d65f0c33b720d17fbe00f1c07dc9956eb
Live4dreamCH <live4dream@stu.xjtu.edu.cn>
2020-03-25 19:51:25 +0800

    6

--- matlab_tran_markdown_simplified.py
+++ matlab_tran_markdown_simplified.py
@@ -1,59 +1,67 @@
 import os
 
 
-Operator = ['+','-','*','/','(',')','^']#仅需要处理的操作符们
+Operator = ["+", "-", "*", "/", "(", ")", "^", "_"]  # 仅需要处理的操作符们
 # + - 不用操作
 # * 变成 ' \times '
 # / 变成 \frac {} {}
 # ^... 变成 ^{...}
 
-an = input('')#输入
+an = input("")  # 输入
 
-#提取出空格并删除
-an=an.replace(' ','')
+# 提取出空格并删除
+an = an.replace(" ", "")
 
-#获取SPLIT数组，按照操作符拆分
-tmp = ''
+# 获取SPLIT数组，按照操作符拆分
+tmp = ""
 SPLIT = []
 for x in an:
-    if(x in Operator):
-        if(tmp!=''):
+    if x in Operator:
+        if tmp != "":
             SPLIT.append(tmp)
         SPLIT.append(x)
-        tmp=''
+        tmp = ""
     else:
-        tmp = tmp+(x)
-if(tmp!=''):
+        tmp = tmp + (x)
+if tmp != "":
     SPLIT.append(tmp)
-    tmp=''
+    tmp = ""
+
 
 def H(ch):
-    if(ch[-1] == '('):
+    """优先级"""
+    if ch[-1] == "(":
         return 0
-    if(ch == '+' or ch == '-'):
+    if ch == "+" or ch == "-":
         return 1
-    if(ch == '*' or ch == '/'):
+    if ch == "*" or ch == "/":
         return 2
-    if(ch == '^'):
-        return 3;
-    if(ch == ')'):
+    if ch == "^" or ch == "_":
+        return 3
+    if ch == ")":
         return 4
-    return -1;
+    return -1
 
-def OPE(Num1,Opt,Num2):
-    if(Opt == '*'):
-        return Num1 + ' \\times ' + Num2
-    if(Opt == "/"):
-        return '{\\frac{' + Num1 + '}{' + Num2 + '}}'
-    if(Opt == '^'):
-        if Num2 == '({\\frac{1}{2}})':
-            return '\sqrt{' + Num1 + '}'
+
+def OPE(Num1, Opt, Num2):
+    """运算符转化"""
+    if Opt == "*":
+        return Num1 + " \\times " + Num2
+    if Opt == "/":
+        return "{\\frac{" + Num1 + "}{" + Num2 + "}}"
+    if Opt == "^":
+        if Num2 == "({\\frac{1}{2}})":
+            return "\sqrt{" + Num1 + "}"
         else:
-            return Num1 + '^{' + Num2 + '}'
+            return "{" + Num1 + "}^{" + Num2 + "}"
+    if Opt == "_":
+        return "{" + Num1 + "}_{" + Num2 + "}"
     return Num1 + Opt + Num2
+
 
 OptStk = []
 NumStk = []
+
 
 def DO():
     a = NumStk[-1]
@@ -62,35 +70,35 @@ def DO():
     NumStk.pop()
     c = OptStk[-1]
     OptStk.pop()
-    New_Number = OPE(b,c,a)
+    New_Number = OPE(b, c, a)
     NumStk.append(New_Number)
-    
-#将SPLIT数组转换为markdown语言
+
+
+# 将SPLIT数组转换为markdown语言
 
 for x in SPLIT:
-    if(H(x) == -1):
+    if H(x) == -1:
         NumStk.append(x)
     else:
-        if(H(x) == 0):
+        if H(x) == 0:
             OptStk.append(x)
             continue
-        if(H(x) == 4):
-            while(H(OptStk[-1]) != 0):
+        if H(x) == 4:
+            while H(OptStk[-1]) != 0:
                 DO()
-            NumStk[-1] = OptStk[-1] + NumStk[-1] + ')'
+            NumStk[-1] = OptStk[-1] + NumStk[-1] + ")"
             OptStk.pop()
-            continue;
+            continue
         while 1:
-            if(len(OptStk) == 0):
-                break;
-            if(H(OptStk[-1]) < H(x)):
-                break;
+            if len(OptStk) == 0:
+                break
+            if H(OptStk[-1]) < H(x):
+                break
             DO()
         OptStk.append(x)
-        
-while (len(OptStk) != 0):
+
+while len(OptStk) != 0:
     DO()
-ans = '$$ '+NumStk[-1]+' $$'
+ans = "$$ " + NumStk[-1] + " $$"
 
 print(ans)
-
--- new.md
+++ new.md
@@ -2,4 +2,17 @@
 
 $ \frac{dU_{CEQ}}{d\beta}=$
 ${\frac{((R_C \times \beta+R_E \times (\beta+1)) \times ({\frac{(R_E \times (E-U_{CEQ}))}{(R_C \times \beta+R_E \times (\beta+1))}}-{\frac{((R_C+R_E) \times (E-U_{CEQ}) \times (R_E \times (\beta+1)+{\frac{1}{R_{B1}}}+{\frac{1}{R_{B2}}}))}{(R_C \times \beta+R_E \times (\beta+1))^{2}}}))}{(R_E \times (\beta+1)+{\frac{1}{R_{B1}}}+{\frac{1}{R_{B2}}})}} $
+
+a_be+C^rX-(23645_a.6+35976/x_Yz^rb*3)
+$$ a_{be}+C^{rX}-(23645_{a.6}+{\frac{35976}{x_{Yz}^{rb}}} \times 3) $$
+
+a1^b2^c3
+$$ {{a1}^{b2}}^{c3} $$
+
+a1/b2/c3
+$$ {\frac{{\frac{a1}{b2}}}{c3}} $$
+
+a1/(b2/c3)
+$$ {\frac{a1}{({\frac{b2}{c3}})}} $$
+
 </font>
